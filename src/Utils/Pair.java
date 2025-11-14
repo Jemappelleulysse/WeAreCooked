0626bd7114d7b73765c7268bb5479d96d58dca90
JeremNcl <jerem57ncl@gmail.com>
2025-11-14 16:18:03 +0100

    Structuration de la classe Pair

--- src/Utils/Pair.java
+++ src/Utils/Pair.java
@@ -3,8 +3,13 @@ package Utils;
 import java.util.ArrayList;
 
 public class Pair {
+
     public int i;
     public int j;
+
+    /// //////////// ///
+    /// CONSTRUCTORS ///
+    /// //////////// ///
     public Pair(int i,int j) {
         this.i = i;
         this.j = j;
@@ -14,6 +19,9 @@ public class Pair {
         this.j = ij.j;
     }
 
+    /// /////// ///
+    /// METHODS ///
+    /// /////// ///
     public Pair sub(Pair kl) {
         this.i -= kl.i;
         this.j -= kl.j;
@@ -21,27 +29,32 @@ public class Pair {
     }
 
     public static ArrayList<Pair> coordsToDirections(ArrayList<Pair> path) {
-        if (path.size() < 2)  return new ArrayList<>();
+        if (path.size() < 2) {
+            return new ArrayList<>();
+        }
+
         ArrayList<Pair> result = new ArrayList<>();
         for (int i = 1 ; i < path.size() ; i++) {
             result.add(new Pair( new Pair(path.get(i-1)).sub(path.get(i))));
         }
+
         return result;
+    }
+
+    public static int getTableau(Pair ij, int[][] tableau) {
+        return tableau[ij.i][ij.j];
     }
 
     @Override
     public String toString() {
         return "(" + i +"," + j + ")";
     }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Pair pair = (Pair) o;
         return i == pair.i && j == pair.j;
-    }
-
-    public static int getTableau(Pair ij,int[][] tableau) {
-        return tableau[ij.i][ij.j];
     }
 }
