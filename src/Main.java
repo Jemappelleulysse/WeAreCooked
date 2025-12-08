41c6eb368194a8473d1001d6aad4818dad7ceb3c
Paulo Lucas <lucas.paulo@etu.univ-orleans.fr>
2025-12-08 19:00:44 +0100

    Correction fin de partie

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Model model = new Model(5);
+        Model model = new Model();
         View view = new View();
         Controller controller = new Controller(model, view);
 
