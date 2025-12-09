6f8403a4e5af3ea34d31df696bf35b17626842a1
JeremNcl <jerem57ncl@gmail.com>
2025-12-09 01:38:40 +0100

    Clean

--- src/Recipes/Recipe.java
+++ src/Recipes/Recipe.java
@@ -21,27 +21,6 @@ public abstract class Recipe {
         return ingredients;
     }
 
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || o.getClass() != ArrayList.class) return false;
-//        ArrayList<Ingredient> ingredients = (ArrayList<Ingredient>) o;
-//        if(ingredients.size() != this.ingredients.size()) return false;
-//        for (Ingredient ingredient : ingredients) {
-//            boolean ingredientFound = false;
-//            for (Ingredient ingredient2 : ingredients) {
-//                if (ingredient.equals(ingredient2)) {
-//                    ingredientFound = true;
-//                    break;
-//                }
-//            }
-//            if (!ingredientFound) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-
     public boolean isComplete(ArrayList<Ingredient> validIngredients) {
 
         if (validIngredients.size() != ingredients.size()) return false;
